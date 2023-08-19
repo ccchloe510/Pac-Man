38f11f477f238b73be7c9306bf4576b24155d5ff
Tingxu Chen <115492152+ccchloe510@users.noreply.github.com>
2023-08-19 15:58:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,84 +1,59 @@
 // script.js
-const svg = document.querySelector('.maze');
+const mazeRows = 24;
+const mazeColumns = 24;
 
-// Define your maze walls using SVG path syntax
-const mazeWalls = "M50 50 L350 50 L350 350 L50 350 Z M100 100 L300 100 L300 300 L100 300 Z";
+const maze = [];
 
-// Set Pac-Man's initial position and path
-let pacManX = 75;
-let pacManY = 75;
-let pacManPathData = `M${pacManX} ${pacManY}`;
-
-function updatePacManPath() {
-    pacManPath.setAttribute("d", pacManPathData);
+for (let y = 0; y < mazeRows; y++) {
+    const row = [];
+    for (let x = 0; x < mazeColumns; x++) {
+        if (x === 0 || x === mazeColumns - 1 || y === 0 || y === mazeRows - 1) {
+            row.push(1);
+        } else {
+            row.push(Math.random() < 0.7 ? 0 : 1);
+        }
+    }
+    maze.push(row);
 }
 
-// Function to update Pac-Man's position and path based on movement
-function movePacMan(dx, dy) {
-    pacManX += dx;
-    pacManY += dy;
-    pacManPathData += ` L${pacManX} ${pacManY}`;
-    updatePacManPath();
+function generateMaze() {
+    const gameContainer = document.querySelector(".game-container");
+    for (let y = 0; y < maze.length; y++) {
+        for (let x = 0; x < maze[y].length; x++) {
+            const cell = document.createElement("div");
+            cell.className = maze[y][x] === 1 ? "wall" : "path";
+            gameContainer.appendChild(cell);
+        }
+    }
 }
 
-// Handle Pac-Man's movement using arrow keys
+generateMaze();
+
+const pacmanSpeed = 25;
+const pacman = document.querySelector(".pacman");
+
 document.addEventListener("keydown", (event) => {
-    switch (event.key) {
-        case "ArrowUp":
-            movePacMan(0, -10);
-            break;
-        case "ArrowDown":
-            movePacMan(0, 10);
-            break;
-        case "ArrowLeft":
-            movePacMan(-10, 0);
-            break;
-        case "ArrowRight":
-            movePacMan(10, 0);
-            break;
+    if (event.key === "ArrowUp") {
+        console.log("move up");
+        pacman.style.top = `${parseInt(pacman.style.top) - pacmanSpeed}px`;
+    } else if (event.key === "ArrowDown") {
+        console.log("move down");
+        pacman.style.top = `${parseInt(pacman.style.top) + pacmanSpeed}px`;
+    } else if (event.key === "ArrowLeft") {
+        console.log("move left");
+        pacman.style.left = `${parseInt(pacman.style.left) - pacmanSpeed}px`;
+    } else if (event.key === "ArrowRight") {
+        console.log("move right");
+        pacman.style.left = `${parseInt(pacman.style.left) + pacmanSpeed}px`;
     }
 });
 
-// Initialize the maze and Pac-Man's path
-updatePacManPath();
-
-// Set the maze walls
-const mazeWall = document.createElementNS("http://www.w3.org/2000/svg", "path");
-mazeWall.setAttribute("d", mazeWalls);
-mazeWall.setAttribute("fill", "#555"); // Customize the wall color
-svg.appendChild(mazeWall);
 
 
 
-// Initialize the game board and render Pac-Man and ghosts
-function initializeGame() {
-    // Create and render the game board cells
-    // Render Pac-Man and ghosts
 
-    // Set up keyboard event listeners for controlling Pac-Man's movement
-}
 
-// Update Pac-Man's position and handle collisions
-function updateGame() {
-    // Update Pac-Man's position based on the direction
-    // Handle collisions with walls and dots
-    // Update ghost positions
-    // Check for collisions between Pac-Man and ghosts
-    // Update the score
 
-    // Render the updated game board
-}
 
-// Main game loop
-function gameLoop() {
-    updateGame();
-    requestAnimationFrame(gameLoop);
-}
 
-// Start the game loop when the page loads
-window.onload = function () {
-    createGameBoard();
-    initializeGame();
-    gameLoop();
-};
 
